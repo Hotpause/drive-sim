8a8e453f0c7ec20135529f2a266019b06ce8a249
Hotpause <ashutoshagarwal1006@gmail.com>
2024-07-29 15:15:54 +0530

    add css

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.set("view engine", "ejs"); // New
-app.set("views", path.join(__dirname, "views")); // New
+app.use(express.static(path.join(__dirname, "src")));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(
   session({
