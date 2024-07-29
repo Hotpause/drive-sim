c3f1b6b35992d8e35d1c3a9b3300eb693a378ca5
Hotpause <ashutoshagarwal1006@gmail.com>
2024-07-29 15:18:57 +0530

    add css

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "src")));
+app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
